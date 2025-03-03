3331ed2e363e6f93b9041431e0487b9962a602c5
Felipe Brum <fbrump@gmail.com>
2025-03-03 22:02:55 +0000

    feat: Create Produc Model
    - Create a new model
    - Add admin setup
    
    ticket: #2

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    empty_value_display = "-empty-"
+
+    list_display = ["code", "name", "price", "price_currency", "is_active", "created_at", "updated_at"]
+    list_filter = ["is_active", "price_currency"]
+    list_per_page = 50
+    ordering = ["name", "created_at",]
+    
+    search_fields = ["name", "code",]
+    exclude = ["created_at", "updated_at", "created_by"]
+    fields = [
+        ("code","name"),
+        ("price_currency","price"),
+        "is_active",
+        ("created_at", "updated_at", "created_by",),]
+    readonly_fields = ["created_at", "updated_at", "created_by"]
+    def save_model(self, request, obj, form, change):
+        if (not change):
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Product, ProductAdmin)
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    class CurrencyOptions(models.TextChoices):
+        EURO = 'EUR'
+        DOLLAR = 'USD'
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=30, unique=True, db_index=True)
+    name = models.CharField(max_length=60, null=False, blank=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=16, decimal_places=2)
+    price_currency = models.CharField(
+        max_length=3,
+        choices=CurrencyOptions,
+        default=CurrencyOptions.EURO,
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
