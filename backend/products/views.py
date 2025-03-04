bd4a97609871bd7b50af26805e33c6922e964892
Brum <fbrump@gmail.com>
2025-03-04 12:52:18 +0000

    feat: Implement a Product Category API (#45)
    - Add new serializer
    - Add new view
    - Add new router URL
    - Add logging support on settings
    - Improve some configurations on Product
    
    ticket: #6

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,19 +1,39 @@
+import logging
 from rest_framework import permissions, viewsets, versioning
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, ProductCategory
+from .serializers import ProductSerializer, ProductCategorySerializer
 
+
+_logger = logging.getLogger(__name__)
 
 class ProductViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows users to view or edit product.
     """
     queryset = Product.objects.all().order_by('-created_at')
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
     versioning_class = versioning.NamespaceVersioning
     def create(self, request):
-        serializer = ProductSerializer(data=request.data)
+        serializer = ProductSerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save(created_by=request.user)
+        return Response(serializer.data)
+
+class ProductCategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to view or edit product category.
+    """
+    queryset = ProductCategory.objects.all().order_by('-created_at')
+    serializer_class = ProductCategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    versioning_class = versioning.NamespaceVersioning
+    def create(self, request) -> Response:
+        _logger.debug('[ Product Category ] Start to create a new category')
+        serializer = ProductCategorySerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(created_by=request.user)
+        _logger.debug('[ Product Category ] Complete to create a new category')
+        _logger.warning('[ Product Category ] Serializer: %s', serializer)
         return Response(serializer.data)
