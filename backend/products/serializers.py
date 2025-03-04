5787f69a589358ee08ecc4bab94a0ee56272eaa1
Felipe Brum <fbrump@gmail.com>
2025-03-04 12:49:14 +0000

    feat: Implement a Product Category API
    - Add new serializer
    - Add new view
    - Add new router URL
    - Add logging support on settings
    - Improve some configurations on Product
    
    ticket: #6

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
-from .models import Product
-from accounts.serializers import UserSerializer
+from .models import Product, ProductCategory
 
+
+class ProductCategorySerializer(serializers.HyperlinkedModelSerializer):
+    is_active = serializers.BooleanField(default=True,)
+    created_by = serializers.SlugRelatedField(many=False,read_only=True, slug_field='username',)
+    class Meta:
+        model = ProductCategory
+        fields = [
+            'id', 'url', 'name', 'description', 'is_active',
+            'created_at', 'updated_at', 'created_by',]
+        read_only_fields = ['id', 'url', 'created_at', 'updated_at',]
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     price = serializers.DecimalField(max_digits=16, decimal_places=2)
     is_active = serializers.BooleanField(default=True,)
-    # created_by = serializers.StringRelatedField(many=False)
-    # created_by = serializers.PrimaryKeyRelatedField(many=False,read_only=True,)
-    # created_by = serializers.SlugRelatedField(many=False,read_only=True, slug_field='username',)
-    # created_by = UserSerializer(many=False, read_only=True)
     created_by = serializers.SlugRelatedField(many=False,read_only=True, slug_field='username',)
     class Meta:
         model = Product
         fields = [
-            'url', 'code', 'name', 'description', 'price', 'price_currency', 'is_active', 
-            'created_at', 'updated_at', 'created_by',]
-        read_only_fields = ['url', 'created_at', 'updated_at',]
+            'id', 'url', 'code', 'name', 'description', 'price', 'price_currency', 'is_active', 
+            'category', 'created_at', 'updated_at', 'created_by',]
+        read_only_fields = ['id', 'url', 'created_at', 'updated_at',]
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -8,5 +8,6 @@ app_name='products'
 router = routers.SimpleRouter()
 
 router.register('products', views.ProductViewSet)
+router.register('categories', views.ProductCategoryViewSet)
 
 urlpatterns = router.urls
