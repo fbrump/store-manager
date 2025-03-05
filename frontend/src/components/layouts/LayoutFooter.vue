be19014bbbe699e0a1e6e3680245ca595a218c3d
Brum <fbrump@gmail.com>
2025-03-05 21:56:12 +0000

    feat: Add footer (#50)
    - Add HTML for the footer of layout
    - Create a component
    - Remove HelloWolrd component
    - Create a layouts folder
    
    ticket: #20 #21

---
+++ frontend/src/components/layouts/LayoutFooter.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const currentYear = ref(new Date().getFullYear());
+
+</script>
+
+<template>
+<footer class="h-80 bg-gray-700 text-center">
+  <p class="m-auto py-2.5 font-medium tracking-wider text-base">
+    <em class="italic">"An investment in knowledge pays the best interest."</em> — Benjamin Franklin
+  </p>
+  <div class="justify-center">
+    <hr class="mx-8" />
+  </div>
+  <p class="my-15 font-semibold">
+    © {{ currentYear }} Brum, Inc. All rights reserved.
+  </p>
+</footer>  
+</template>
+
+<style scoped>
+
+</style>
