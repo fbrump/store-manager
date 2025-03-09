a047b786ec74173e0ed79143ac4a14c8769e3ee8
Felipe Brum <fbrump@gmail.com>
2025-03-09 01:14:20 +0000

    feat: Initial work on table css

--- frontend/src/views/categories/CategoryList.vue
+++ frontend/src/views/categories/CategoryList.vue
@@ -44,38 +44,38 @@ const categories = ref<Array<Category>>([
 <template>
 <main> 
     <view-header title="Categories" subtitle="List of categories"></view-header>
-    <section>
+    <section class="container">
        
-        <table v-if="categories.length > 0">
-            <thead>
+        <table v-if="categories.length > 0" class="border-collapse border border-gray-400 m-5">
+            <thead class="bg-gray-200">
                 <tr>
-                    <th>
+                    <th class="p-0.5 border-collapse border border-gray-400">
                         Name
                     </th>
-                    <th>
+                    <th class="p-0.5 border-collapse border border-gray-400">
                         Description
                     </th>
-                    <th>
+                    <th class="p-0.5 border-collapse border border-gray-400">
                         Active
                     </th>
-                    <th>
+                    <th class="p-0.5 border-collapse border border-gray-400">
                         Actions
                     </th>
                 </tr>
             </thead>
-            <tfoot>
+            <tfoot class="bg-gray-200">
                 <tr>
-                    <td colspan="4">
+                    <td colspan="4" class="py-1 text-center">
                         Total of items {{ categories.length }}.
                     </td>
                 </tr>
             </tfoot>
-            <tbody>
-                <tr v-for="item in categories" :key="item.id">
-                    <td>
+            <tbody class="bg-gray-100">
+                <tr v-for="item in categories" :key="item.id" class="hover:bg-gray-300 h-auto">
+                    <td class="p-0.5 border-collapse border border-gray-400">
                         {{ item.name }}
                     </td>
-                    <td>
+                    <td class="p-0.5 border-collapse border border-gray-400">
                         <p v-if="item.description">
                             {{ item.description }}
                         </p>
@@ -83,11 +83,11 @@ const categories = ref<Array<Category>>([
                             -- Empty --
                         </p>
                     </td>
-                    <td>
+                    <td class="p-0.5 border-collapse border border-gray-400 text-center">
                         <input type="checkbox" name="active" :checked="item.active" />
                     </td>
-                    <td>
-                        <router-link :to="`/categories/${item.id}`">
+                    <td class="p-0.5 border-collapse border border-gray-400 text-center">
+                        <router-link :to="`/categories/${item.id}`" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
                             Details
                         </router-link>
                     </td>
