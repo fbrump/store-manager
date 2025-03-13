9ac45fc993b51ebf14840bff920e0aa3317335aa
Felipe Brum <fbrump@gmail.com>
2025-03-13 22:16:28 +0000

    feat: Create HTML on Category Details
    - Use the view-header component
    - Add html of form
    
    ticket: #11

--- frontend/src/views/categories/CategoryDetail.vue
+++ frontend/src/views/categories/CategoryDetail.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
+import ViewHeader from '../../components/ViewHeader.vue';
 
 </script>
 
 <template>
-<main>
-    <header>
-        <h1>
-            Category {{ $route.params.id }} Details
-        </h1>
-    </header>
+<main class="justify-items-center">
+    <view-header title="Category" subtitle="Show details of the category"></view-header>
+    <section>
+        <form action="" method="post">
+            <div>
+                <label for="name">
+                    Name
+                </label>
+                <input type="text" name="name" id="name" title="Category name" />
+            </div>
+            <div>
+                <label for="active">Active</label>
+                <input type="checkbox" name="active" id="active" />
+            </div>
+            <div>
+                <label for="description">
+                    Description
+                </label>
+                <textarea name="description" id="description" cols="30" rows="10"></textarea>
+            </div>
+            <button type="submit">
+                Save
+            </button>
+        </form>
+    </section>
 </main>
 </template>
