cc1ed5456e33288387e640d60f02218bfe94e4fc
Felipe Brum <fbrump@gmail.com>
2025-03-16 13:24:02 +0000

    feat: Structure the Product List HTML
    - Add table of registers
    - Add link to show details
    - Add link to add new one
    - Upgrade yarn packages
    
    ticket: #7

--- frontend/src/views/products/ProductList.vue
+++ frontend/src/views/products/ProductList.vue
@@ -1,13 +1,72 @@
 <script setup lang="ts">
+import ViewHeader from '../../components/ViewHeader.vue';
 
 </script>
 
 <template>
 <main>
-    <header>
-        <h1>
-            Products
-        </h1>
-    </header>
+    <view-header title="Products" subtitle="List of products"></view-header>
+    <section>
+        <a href="#/products/new" title="Go to form to insert a new one">
+            Add New
+        </a>
+    </section>
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Code
+                    </th>
+                    <th>
+                       Name 
+                    </th>
+                    <th>
+                       Description 
+                    </th>
+                    <th>
+                       Price 
+                    </th>
+                    <th>
+                       Category 
+                    </th>
+                    <th>
+                       Actions 
+                    </th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <td colspan="6">
+                        Total items <strong>[Number]</strong>.
+                    </td>
+                </tr>
+            </tfoot>
+            <tbody>
+                <tr>
+                    <td>
+                        123
+                    </td>
+                    <td>
+                        Corne V3
+                    </td>
+                    <td>
+                        Split keyboard bluetooth with blue switchers and LP keykaps
+                    </td>
+                    <td>
+                        250.00 Euro
+                    </td>
+                    <td>
+                        Kayboards
+                    </td>
+                    <td>
+                        <a href="#products/123" title="Go to details">
+                            Details
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
 </main>
 </template>
