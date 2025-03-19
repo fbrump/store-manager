a3f4cf4dcf570a6125ba99481e5b4d6f78a93a3c
Felipe Brum <fbrump@gmail.com>
2025-03-19 22:21:07 +0000

    feat: Apply Style on List Products
    - Update CSS Styles
    - Add new store of products
    - Add fake list
    - Add new router to details
    
    ticket: #8

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -5,12 +5,14 @@ import CategoryList from './views/categories/CategoryList.vue'
 import CategoryDetail from './views/categories/CategoryDetail.vue'
 import ProductList from './views/products/ProductList.vue'
 import TransactionList from './views/transactions/TransactionList.vue'
+import ProductDetail from './views/products/ProductDetail.vue';
 
 const routes = [
   { path: '/', component: DashboardIndex },
   { path: '/categories', component: CategoryList },
   { path: '/categories/:id', component: CategoryDetail, props: true },
   { path: '/products', component: ProductList },
+  { path: '/products/:id', component: ProductDetail },
   { path: '/transactions', component: TransactionList },
 ]
 
