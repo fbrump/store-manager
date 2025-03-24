dd1e1b0aebc784c6ad745abb540d54f52dd84008
Brum <fbrump@gmail.com>
2025-03-24 20:51:47 +0000

    feat: Create HTML of Transactions List (#69)
    - Add HTML
    - Use View Header
    
    ticket: #29

--- frontend/src/views/transactions/TransactionList.vue
+++ frontend/src/views/transactions/TransactionList.vue
@@ -1,13 +1,80 @@
 <script setup lang="ts">
+import ViewHeader from '@components/ViewHeader.vue';
 
 </script>
 
 <template>
 <main>
-    <header>
-        <h1>
-            Transactions
-        </h1>
-    </header>
+    <view-header title="Transactions" subtitle="List of transactions"></view-header>
+    <section>
+        <div>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Code 
+                    </th>
+                    <th>
+                        Product
+                    </th>
+                    <th>
+                        Category
+                    </th>
+                    <th>
+                        User
+                    </th>
+                    <th>
+                        Type
+                    </th>
+                    <th>
+                        Amount
+                    </th>
+                    <th>
+                        Actions 
+                    </th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <td colspan="7">
+                        Total items <strong class="font-semibold">0</strong>.
+                    </td>
+                </tr>
+            </tfoot>
+            <tbody>
+                <tr>
+                    <td>
+                        3748290
+                    </td>
+                    <td>
+                        T-Shart
+                    </td>
+                    <td>
+                        Clouth
+                    </td>
+                    <td>
+                        Employee 1
+                    </td>
+                    <td>
+                        In
+                    </td>
+                    <td>
+                        5
+                    </td>
+                    <td>
+                        <a href="#details">
+                            Details
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div> 
+            <h3> 
+                There is no items :(
+            </h3>
+        </div>
+    </section>
 </main>
 </template>
