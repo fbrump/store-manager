2b51d4748d9b5ae614f7ea1be2fbc5f8df620cd9
Brum <fbrump@gmail.com>
2025-04-07 00:08:56 +0100

    feat: Apply CSS on Transaction Details (#75)
    - Refactory detail form csss
    - Apply CSS on style.css
    - Create select icon component
    - Remove CSS from other details
    
    ticket: #32

--- frontend/src/views/categories/CategoryDetail.vue
+++ frontend/src/views/categories/CategoryDetail.vue
@@ -18,47 +18,47 @@ const category = ref<Category | null>(store.getById(props.id));
 <main class="flex flex-col">
     <view-header title="Category" subtitle="Show details of the category"></view-header>
     <section v-if="category" class="justify-items-center">
-        <form action="" method="post" class="w-full max-w-sm bg-white p-4">
-            <div class="md:flex md:items-center mb-6">
+        <form action="" method="post">
+            <div>
                 <div class="md:w-1/3">
-                    <label for="name" class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                    <label for="name">
                         Name
                     </label>
                 </div>
                 <div class="md:w-2/3">
-                    <input v-model="category.name" id="name" name="name" placeholder="Enter with category name" title="Category name" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cyan-500">
+                    <input type="text" v-model="category.name" id="name" name="name" placeholder="Enter with category name" title="Category name" />
                 </div>
             </div>
-            <div class="md:flex md:items-center mb-6">
+            
+            <div> 
+                <div class="md:w-1/3">
+                    <label for="description">
+                       Description 
+                    </label>
+                </div>
+                <div class="md:w-2/3">
+                    <textarea v-model="category.description" id="description" name="description" placeholder="Enter with category description" cols="30" rows="10"></textarea>
+                </div>
+            </div>
+            <div>
                 <div class="md:w-1/3">
                 </div>
-                <label for="active" class="md:w-2/3 block text-gray-500 font-bold">
+                <label for="active" class="md:w-2/3">
                     <input type="checkbox" v-model="category.active" name="active" id="active" class="mr-2 leading-tight" /> 
                     <span class="text-sm">
                         Active 
                     </span>
                 </label>
             </div>
-            <div class="md:flex md:items-center mb-6">
-                <div class="md:w-1/3">
-                    <label for="description" class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                       Description 
-                    </label>
-                </div>
-                <div class="md:w-2/3">
-                    <textarea v-model="category.description" id="description" name="description" placeholder="Enter with category description" cols="30" rows="10" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-cyan-500"></textarea>
-                </div>
-            </div>
-            <div class="md:flex md:items-center mb-6">
+            <div>
                 <div class="md:w-1/3"></div>
                 <div class="md:w-2/3 gap-0.5 text-right">
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-1 rounded-full">
+                    <button type="submit">
                         Save
                     </button>
-                    <router-link to="/categories" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 mx-1 rounded-full" title="Cancel and go back to list">
+                    <router-link to="/categories" title="Cancel and go back to list">
                         Cancel 
                     </router-link>
-                    
                 </div>
             </div>
         </form>
