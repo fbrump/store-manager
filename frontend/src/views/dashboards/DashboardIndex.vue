23b58d952a839775d6ed1f5499ce1698678d1b72
Brum <fbrump@gmail.com>
2025-04-12 21:07:33 +0100

    feat: Create Dashboard HTML (#81)
    - Add html to handle each port of the dashboard
    - Number of transactions
    - Top 5 transactions
    - Products with less amount on stock
    - (Pizza Chart5) Count of Products by Category
    
    ticket: #37

--- frontend/src/views/dashboards/DashboardIndex.vue
+++ frontend/src/views/dashboards/DashboardIndex.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useTransactionsStore  } from '@stores/transactions';
+import type { Transaction } from '@/views/transactions/interfaces';
+
+
+const store = useTransactionsStore();
+
+const transactions = ref<Array<Transaction>>(store.getAll);
+const numberOfTransactions = ref<number>(transactions.value.length);
 
 </script>
 
@@ -9,5 +18,63 @@
             Dashboard Charts
         </h1>
     </header>
+    <section>
+        <h2>
+            There are <strong>{{ numberOfTransactions }}</strong>.
+        </h2>
+    </section>
+    <section>
+        Top 5 Transactions.
+        <div>
+            <ol>
+                <li>
+                    1 Produt Name - In   
+                </li>
+                <li>
+                    2 Produt Name - In   
+                </li>
+                <li>
+                    3 Produt Name - In   
+                </li>
+                <li>
+                    4 Produt Name - In   
+                </li>
+                <li>
+                    5 Produt Name - In   
+                </li>
+            </ol>
+        </div>
+    </section>
+    <section>
+        Products with less amount on stock.
+        <ul>
+            <li>
+                Product Name - <strong>number</strong>
+            </li>
+            <li>
+                Product Name - <strong>number</strong>
+            </li>
+            <li>
+                Product Name - <strong>number</strong>
+            </li>
+            <li>
+                Product Name - <strong>number</strong>
+            </li>
+        </ul>
+    </section>
+    <section>
+        Products by Category.
+        <ul>
+            <li>
+                <strong>x</strong> Products by <em>Category Name</em>
+            </li>
+            <li>
+                <strong>x</strong> Products by <em>Category Name</em>
+            </li>
+            <li>
+                <strong>x</strong> Products by <em>Category Name</em>
+            </li>
+        </ul>
+    </section>
 </main>
 </template>
